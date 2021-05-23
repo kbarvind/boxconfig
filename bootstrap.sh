d2a0d1649341f3fae497f1d2ccb297a0f4de456e
k b, arvind <kb.arvind@gmail.com>
2021-05-23 16:22:33 +0530

    updated boxconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ sudo apt install -y software-properties-common
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 
-sudo apt install -y python3.6 python3-pip python3-setuptools python3-dev build-essential apt-get install
+sudo apt install -y python3.6 python3-pip python3-setuptools python3-dev build-essential
 
 sudo apt-get install -y libkrb5-dev
 
@@ -28,7 +28,7 @@ wget -q https://github.com/kbarvind/boxconfig/raw/main/boxconfig/boxconfig-1.0-p
 
 wget -q https://github.com/kbarvind/boxconfig/raw/main/boxconfig/boxconfig-1.0.tar.gz
 
-pip3 install --upgrade pip
+pip3.6 install --upgrade pip
 
 python3.6 -m easy_install boxconfig-1.0.tar.gz
 
