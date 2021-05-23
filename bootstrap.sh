7260ce1e34c4995af9ea5eceffc84bbf25f7b2d1
k b, arvind <kb.arvind@gmail.com>
2021-05-23 11:24:13 +0530

    updated bootstrap sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,19 +8,24 @@ sudo apt install -y software-properties-common
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 
-sudo apt install -y python3.6 python3-pip python-setuptools python-setuptools-devel
+sudo apt install -y python3.6 python3-pip python-setuptools
 
-echo "Downloading boxconfig "
+echo "Creating temporary folder for downloadables"
 
 mkdir -p /tmp/boxconfig/
 
+rm -rf /tmp/boxconfig/*
+
 cd /tmp/boxconfig/
+
+echo "Downloading boxconfig "
 
 wget https://github.com/kbarvind/boxconfig/raw/main/boxconfig/boxconfig-1.0-py3-none-any.whl
 
 wget https://github.com/kbarvind/boxconfig/raw/main/boxconfig/boxconfig-1.0-py3.7.egg
 
 wget https://github.com/kbarvind/boxconfig/raw/main/boxconfig/boxconfig-1.0.tar.gz
+
 
 
 
